230896bfd4cbe0f38421b31f45e321800dc0f0c7
Brennan Heisler <learn@heislercreative.com>
2018-01-25 13:41:10 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
 
+def sort_array_desc(numbers)
+  numbers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+# def swap_elements(array)
+#   array.sort do |index|
+#     index[2] <=> index[1]
+#   end
+# end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(strings)
+  strings.each { |kesha| kesha[2] = "$"}
+end
+
+def find_a(array)
+  array.select { |string| string.start_with?("a") }
+end
+
+def sum_array(numbers)
+  numbers.sum
+end
+
+def add_s(words)
+  words.each_with_index.collect do |word, index|
+    if index != 1
+      word + "s"
+  end
+end
